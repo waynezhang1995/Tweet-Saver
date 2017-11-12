59032fd9ebf0a9c12229da20bd004353046c86ce
Wayne Zhang <waynezhangyuwei@Gmail.com>
2017-11-12 14:04:39 -0800

    update

--- tweet-saver-module/search.component.js
+++ tweet-saver-module/search.component.js
@@ -1,23 +1,23 @@
-(function(){
-"use strict";
+(function() {
+    "use strict";
 
-var tweetSaver = angular.module("tweetSaverModule");
+    var tweetSaver = angular.module("tweetSaverModule");
 
-var search = tweetSaver.component("search", {
-	templateUrl: "tweet-saver-module/search.component.html",
-	bindings: {},
-	controllerAs: "model",
-	controller: function(){
-		var model = this;
+    var search = tweetSaver.component("search", {
+        templateUrl: "tweet-saver-module/search.component.html",
+        bindings: {},
+        controllerAs: "model",
+        controller: function() {
+            var model = this;
 
-		model.$onInit = function(){
+            model.$onInit = function() {
 
-		}
+            }
 
-		model.$onChange = function(){
+            model.$onChange = function() {
 
-		}
-	}
-});
+            }
+        }
+    });
 
 }());
