b3a2383171ec2ebfac24b0e78c8b9bcf76f431cd
Wayne Zhang <waynezhangyuwei@Gmail.com>
2017-11-12 14:55:40 -0800

    update

--- tweet-saver-module/tweet-detail.component.js
+++ tweet-saver-module/tweet-detail.component.js
@@ -11,6 +11,22 @@
             var model = this;
 
             model.$onInit = function() {
+                $.ajax({
+                    url: 'http://tweetsaver.herokuapp.com/?q=obama&callback=yourJSONPCallbackFn&count=10',
+                    type: 'GET',
+                    contentType: "application/json; charset=utf-8",
+                    success: function(response) {
+                        var result = JSON.parse(response);
+                        console.log(result);
+                    },
+                    error: function() {
+                        alert('error');
+                    }
+                });
+                model.userName = "Simon";
+                model.userAccount = "sbhahh";
+                model.tweetDate = "2019";
+                model.tweetContent = "aasda";
 
             }
 
--- tweet-saver-module/tweets-display.component.js
+++ tweet-saver-module/tweets-display.component.js
@@ -1,23 +1,23 @@
-(function(){
-"use strict";
+(function() {
+    "use strict";
 
-var tweetSaver = angular.module("tweetSaverModule");
+    var tweetSaver = angular.module("tweetSaverModule");
 
-var tweetsDisplay = tweetSaver.component("tweetsDisplay", {
-	templateUrl: "",
-	bindings: {},
-	controllerAs: "model",
-	controller: function(){
-		var model = this;
+    var tweetsDisplay = tweetSaver.component("tweetsDisplay", {
+        templateUrl: "tweet-saver-module/tweets-display.component.html",
+        bindings: {},
+        controllerAs: "model",
+        controller: function() {
+            var model = this;
 
-		model.$onInit = function(){
+            model.$onInit = function() {
 
-		}
+            }
 
-		model.$onChange = function(){
-		
-		}
-	}
-});
+            model.$onChange = function() {
+
+            }
+        }
+    });
 
 }());
